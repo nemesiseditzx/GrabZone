e7036c63cddf730998f3f6795540e344dbcf21d2
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-08-23 14:03:42 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.STORE_DEFAULTS = {
-  name: "GRABZONE",
-  currency: "৳",
-  whatsapp: "https://wa.me/15551234567",
-  messenger: "https://m.me/yourpage",
-  instagram: "https://instagram.com/yourstore",
-  adminUsername: "admin",
-  adminPassword: "change-me"
-};
