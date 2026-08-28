c288efb77e4a8f4eb3a43a987944351cd8662818
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-08-28 16:36:09 -0400

    Configure default COD shipping charge

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ window.GRABZONE_CONFIG = {
   storeName: "GRABZONE",
   tagline: "Grab What's Trending.",
   currency: "৳",
+  shippingCharge: 130,
   whatsapp: "https://wa.me/8801XXXXXXXXX",
   messenger: "https://m.me/yourpage",
   instagram: "https://instagram.com/yourstore"
