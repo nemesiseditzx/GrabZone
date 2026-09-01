f38e5ee93eaf509454b382312d7290a93ea44809
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-08-31 21:18:14 -0400

    Fix admin panels to use D1 client bridge

--- referrals-admin.js
+++ referrals-admin.js
@@ -1,8 +1,7 @@
 (() => {
 'use strict';
 const C=window.GRABZONE_CONFIG||{};
-const sb=window.supabase&&C.supabaseUrl&&!String(C.supabaseUrl).includes('PASTE_')
-  ?window.supabase.createClient(C.supabaseUrl,C.supabaseAnonKey):null;
+const sb=window.grabzoneD1||null;
 const $=id=>document.getElementById(id);
 const esc=v=>String(v??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m]));
 let editingId=null,referrals=[];
