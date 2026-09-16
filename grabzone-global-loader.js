ff38a5ff223621dc4cf688920585f86cd1065531
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-09-15 22:07:28 -0400

    Bump notice sync cache version

--- grabzone-global-loader.js
+++ grabzone-global-loader.js
@@ -69,7 +69,7 @@
 
   try{
     var ns=document.createElement('script');
-    ns.src='/grabzone-notice-sync.js?v=20260916-notice4';
+    ns.src='/grabzone-notice-sync.js?v=20260916-notice5';
     ns.defer=true;
     ns.setAttribute('data-grabzone-notice-sync','true');
     (document.head||document.documentElement).appendChild(ns);
